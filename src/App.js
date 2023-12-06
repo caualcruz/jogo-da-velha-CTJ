c2f2e97a8176d6994a34ee3204ffb07c4c2f6276
Cauã Cruz <caualucas34492@gmail.com>
2023-12-06 19:06:34 -0300

    asdasf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import Button from '@mui/material/Button';
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"></link>
 
 function  Square({ value, onSquareClick }) {
   return (
